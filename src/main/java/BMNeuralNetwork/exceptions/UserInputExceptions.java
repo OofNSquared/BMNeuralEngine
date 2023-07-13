fed4ffcda8517d252c29e14856466c784eb24140
Chris Sison <helloworld@OofNSquared.local>
2023-07-13 00:19:47 -0700

    Clean up

--- src/main/java/BMNeuralNetwork/exceptions/UserInputExceptions.java
+++ src/main/java/BMNeuralNetwork/exceptions/UserInputExceptions.java
@@ -2,20 +2,14 @@ package BMNeuralNetwork.exceptions;
 
 public class UserInputExceptions {
     public static class BlankFieldException extends Exception {
-        public BlankFieldException(String message) {
-            super(message);
-        }
+        public BlankFieldException(String message) { super(message); }
     }
 
     public static class EmailAlreadyExistsException extends Exception {
-        public EmailAlreadyExistsException(String message) {
-            super(message);
-        }
+        public EmailAlreadyExistsException(String message) { super(message); }
     }
 
     public static class InvalidPasswordException extends Exception {
-        public InvalidPasswordException(String message) {
-            super(message);
-        }
+        public InvalidPasswordException(String message) { super(message); }
     }
 }
--- src/main/java/BMNeuralNetwork/users/UserTemplate/User.java
+++ src/main/java/BMNeuralNetwork/users/UserTemplate/User.java
@@ -6,9 +6,7 @@ public class User {
     private int insuranceId;
     private String firstName;
     private String lastName;
-
     // private int socialSecurity;
-
     private String username;
     private String password;
     private String email;
@@ -26,29 +24,12 @@ public class User {
     }
 
     public User(String firstName, String lastName, String username, String password, String email, boolean isPatient) throws UserInputExceptions.EmailAlreadyExistsException, UserInputExceptions.BlankFieldException, UserInputExceptions.InvalidPasswordException {
-        if (firstName == null || firstName.isEmpty()) {
-            throw new UserInputExceptions.BlankFieldException("First name cannot be left blank.");
-        }
-
-        if (lastName == null || lastName.isEmpty()) {
-            throw new UserInputExceptions.BlankFieldException("Last name cannot be left blank.");
-        }
-
-        if (username == null || username.isEmpty()) {
-            throw new UserInputExceptions.BlankFieldException("Username cannot be left blank.");
-        }
-
-
-        if (password == null || password.length() < 6) {
-            throw new UserInputExceptions.InvalidPasswordException("Password must contain at least 6 characters.");
-        }
-
-
-        if (email == null || email.isEmpty()) {
-            throw new UserInputExceptions.BlankFieldException("Email cannot be left blank.");
-        } else if (email.contains("myemail.com")) {
-            throw new UserInputExceptions.EmailAlreadyExistsException("This email already exists.");
-        }
+        if (firstName == null || firstName.isEmpty()) throw new UserInputExceptions.BlankFieldException("First name cannot be left blank.");
+        if (lastName == null || lastName.isEmpty()) throw new UserInputExceptions.BlankFieldException("Last name cannot be left blank.");
+        if (username == null || username.isEmpty()) throw new UserInputExceptions.BlankFieldException("Username cannot be left blank.");
+        if (email == null || email.isEmpty()) throw new UserInputExceptions.BlankFieldException("Email cannot be left blank.");
+        if (email.contains("myemail.com")) throw new UserInputExceptions.EmailAlreadyExistsException("This email already exists.");
+        if (password == null || password.length() < 6) throw new UserInputExceptions.InvalidPasswordException("Password must contain at least 6 characters.");
 
         this.firstName = firstName;
         this.lastName = lastName;
@@ -57,59 +38,23 @@ public class User {
         this.email = email;
         this.isPatient = isPatient;
     }
+
     // Getters
-    public int getInsuranceId() {
-        return insuranceId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public boolean isPatient() {
-        return isPatient;
-    }
+    public int getInsuranceId() { return insuranceId; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public String getEmail() { return email; }
+    public boolean isPatient() { return isPatient; }
 
     // Setters
     public void setInsuranceId(int insuranceId) { this.insuranceId = insuranceId; }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void isPatient(boolean isPatient) {
-        this.isPatient = isPatient;
-    }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setUsername(String username) { this.username = username; }
+    public void setPassword(String password) { this.password = password; }
+    public void setEmail(String email) { this.email = email; }
+    public void isPatient(boolean isPatient) { this.isPatient = isPatient; }
 }
 
